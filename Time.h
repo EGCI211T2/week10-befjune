b67bff3eb61e2e303f75b2c975ca0e8183e3e1e2
befjune <may.renuka.ch@gmail.com>
2026-03-16 08:30:09 +0000

    draft

--- Time.h
+++ Time.h
@@ -8,3 +8,35 @@ struct Time{
 //1.  getTime function
 //2. subtract fuctions
 //3. display functions
+
+void getTime(struct &Time t){
+	cout<<"Input Time(h m s)";
+	cin>>t.h>>t.m>>t.s;
+}
+
+struct Time subtract (struct Time t2, struct Time t1){
+	struct Time t3;// t3=t2-t1
+	/*calculate time*/
+	t3.s=t2.s-t1.s;
+	t3.m=t2.m-t1.s;
+	t3.h=t2.h-t1.h;
+
+	if(t3.s<0){
+		t3.s = t3.s+60;
+		t3.m = t3.m-1;
+	}
+
+	if(t3.m<0){
+		t3.m = t3.m + 60;
+		t3.h = t3.h-1;
+	}
+
+	if(t3.h<0){
+		t3.h = t3.h+12;
+	}
+	return t3;
+}
+
+void display(struct &Time t){
+	cout<<setw(2)<<t3.h<<":"<<t3.m<<":"<<t3.s;
+}
--- main.cpp
+++ main.cpp
@@ -2,16 +2,39 @@
 #include <iomanip>
 
 using namespace std;
-
 #include "Time.h"
 
+void square(int&);
+
+void square(int &x){
+    x*=x;
+}
+
+
 int main(){
+
+    /*int age;
+    string color;
+
+    std::cout<<"Hello world"<<std::endl;
+    cout<<"Your age and color ";
+    cin>>age>>color;
+    square(age);
+    //cout<<"Age: "<<setfill('x')<<setw(8)<<age<<endl;
+    cout<<"Age: "<<fixed<<setprecision(2)<<age<<endl;
+    cout<<"Colour: "<<setfill('x')<<setw(8)<<color<<endl;*/
+
+    
     struct Time t1,t2,t3;
+
     cout<<"What time was it?";
     getTime(t1);
+
     cout<<"What time is it now?";
     getTime(t2);
+
     t3=subtract(t2,t1); //t3=t2-t1
+
     cout<<"Time diff is ";
     display(t3);
 }
